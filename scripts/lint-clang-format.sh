8d8bdf3ebca76ef7c72b901ebf63833cd26c6ef0
Denis Glazkov <glazzk.off@mail.ru>
2024-03-08 23:32:12 +0300

    ci: refactor clang-tidy and clang-format linters

--- scripts/lint-clang-format.sh
+++ scripts/lint-clang-format.sh
@@ -5,6 +5,9 @@ set -e
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_ROOT=$(dirname "$SCRIPT_PATH")
 PROJECT_ROOT=$(dirname "$SCRIPT_ROOT")
-SOURCES_ROOT=$PROJECT_ROOT/gdbus-c++
+SOURCES_ROOT="$PROJECT_ROOT/gdbus-c++"
+EXAMPLE_ROOT="$PROJECT_ROOT/example"
 
-find "$SOURCES_ROOT" -iname '*.hpp' -o -name '*.cpp' -exec clang-format-15 --dry-run -Werror {} +;
+find "$SOURCES_ROOT" "$EXAMPLE_ROOT" \
+    -regex '.+\.[hc]pp' \
+    -exec clang-format-15 --dry-run -Werror {} +;
--- scripts/lint-clang-tidy.sh
+++ scripts/lint-clang-tidy.sh
@@ -5,9 +5,18 @@ set -e
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_ROOT=$(dirname "$SCRIPT_PATH")
 PROJECT_ROOT=$(dirname "$SCRIPT_ROOT")
-SOURCES_ROOT=$PROJECT_ROOT/gdbus-c++
-BUILD_ROOT="$PROJECT_ROOT"/builddir
+SOURCES_ROOT="$PROJECT_ROOT/gdbus-c++"
+EXAMPLE_ROOT="$PROJECT_ROOT/example"
+BUILD_ROOT="$PROJECT_ROOT/builddir"
 
-meson setup --reconfigure "$BUILD_ROOT" "$PROJECT_ROOT"
+if [ -d "$BUILD_ROOT" ]; then
+    rm -rf "$BUILD_ROOT"
+fi
 
-clang-tidy-15 -p "$BUILD_ROOT" "$SOURCES_ROOT"/*.cpp "$SOURCES_ROOT"/*.hpp
+meson setup \
+    -DGDBUS_CPP_BUILD_EXAMPLE=true \
+    -DGDBUS_CPP_ENABLE_DEBUG_LOGGING=true \
+    "$BUILD_ROOT" \
+    "$PROJECT_ROOT"
+
+clang-tidy-15 -p "$BUILD_ROOT" $(find "$SOURCES_ROOT" "$EXAMPLE_ROOT" -regex '.+\.[hc]pp')
